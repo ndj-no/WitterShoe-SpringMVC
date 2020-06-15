041f172939799e3d989e7aa01417a2ee41f1b04f
ndj-no <giang.ndj@gmail.com>
2020-06-15 14:29:39 +0700

    add log4j

--- src/main/java/ndj/no/controllers/HomeController.java
+++ src/main/java/ndj/no/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package ndj.no.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private static final Logger logger = Logger.getLogger(HomeController.class);
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
         return "home";
     }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -6,11 +6,13 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Home Page</title>
 </head>
 <body>
 <h1>Hello World</h1>
+
 </body>
 </html>
